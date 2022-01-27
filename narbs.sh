0b704c4f5070a2ab6af532abe42ec989ecade3c8
nonetrix <45698918+nonetrix@users.noreply.github.com>
2022-01-27 16:12:50 +0000

    Update narbs.sh

--- narbs.sh
+++ narbs.sh
@@ -225,6 +225,9 @@ git update-index --assume-unchanged "/home/$name/README.md" "/home/$name/LICENSE
 # Most important command! Get rid of the beep!
 systembeepoff
 
+# Setup user files 
+xdg-user-dirs-update
+
 # Make zsh the default shell for the user.
 chsh -s /bin/zsh "$name" >/dev/null 2>&1
 sudo -u "$name" mkdir -p "/home/$name/.cache/zsh/"
